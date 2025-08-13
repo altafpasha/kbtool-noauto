d8358728c2c940e437499c4edad6a23da195ed1e
altafpasha <codesec@CodeSec-Zero.localdomain>
2025-08-13 11:34:51 +0530

    update

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -3,5 +3,5 @@ import { sendResumeSignal, setSiteDownFlag } from './socket';
 export default async function handler(req, res) {
   await setSiteDownFlag(false); // Remove "down" flag
   sendResumeSignal();     // Tell open tabs to reload
-  res.status(200).json({ message: 'Site resumed' });
+  res.redirect(302, '/');
 }
